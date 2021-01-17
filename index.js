0ab23ebb9e92f13d8f8c7ff93dd813b17f2321fe
builder_247 <14019974+builder-247@users.noreply.github.com>
2021-01-18 01:17:22 +0200

    and -> or

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-if (typeof process !== 'undefined' && parseInt(process.versions.node.split('.')[0]) < 14) {
+if (typeof process !== 'undefined' || parseInt(process.versions.node.split('.')[0]) < 14) {
   console.error('Your node version is currently', process.versions.node)
   console.error('Please update it to a version >= 14.x.x from https://nodejs.org/')
   process.exit(1)
